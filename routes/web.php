63d87ac7b2d6f72d502b5cdbda810eb31e6bb43b
Kazimieras Stonkus <kazstonk@gmail.com>
2024-04-13 00:14:55 +0300

    Add vue integration

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get( '/{path?}', function(){
+    return view( 'welcome' );
+} )->where('path', '.*');
 Route::get('/{slug}', [\App\Http\Controllers\ShortcutUrlController::class, 'shortcutRedirect']);
